7402daa1d54cfdc86fba379f416722a2fc8b9818
Keelan Fischer <keelan.fischer@gmail.com>
2016-05-14 14:35:58 +1000

    Add SASS compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,17 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'static/css/',
+                    src: ['*.scss'],
+                    dest: 'static/css/',
+                    ext: '.css'
+                }]
+            }
+        },
         cssmin: {
             target: {
                 files: [{
@@ -15,9 +26,10 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['sass', 'cssmin']);
 
 };
