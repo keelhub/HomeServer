261c80110bc16d36d48cd896deca825e7e888fa0
Keelan Fischer <keelan@saltwatersolutions.com.au>
2016-05-19 17:35:08 +1000

    Add tests for comments app Namespace for urls

--- HomeServer/urls.py
+++ HomeServer/urls.py
@@ -10,7 +10,8 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
+    1. Import the include() function: from django.conf.urls import url,
+    include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
@@ -37,15 +38,16 @@ class UserViewSet(viewsets.ModelViewSet):
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'comments', CommentViewSet)
+router.register('users', UserViewSet)
+router.register('comments', CommentViewSet)
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(router.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html'),
+        name='index'),
+    url(r'^admin/', admin.site.urls, name='admin'),
+    url(r'^api/', include(router.urls, namespace='api')),
     url(r'^api/', include('rest_framework.urls')),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^polls/', include('polls.urls')),
-    url(r'^api/snippets', include('snippets.urls')),
+    url(r'^polls/', include('polls.urls'), name='polls'),
+    url(r'^api/snippets', include('snippets.urls'), name='snippets'),
 ]
--- comments/tests.py
+++ comments/tests.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Comment
+
+
+class TestComment(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test_user',
+            password='123'
+        )
+
+    def test_create_comment(self):
+        comment = Comment.objects.create(owner=self.user, title='test')
+        comment.save()
+        self.assertIsInstance(comment, Comment)
+        self.assert_(comment.created)
+
+
+class TestViews(TestCase):
+    def test_get_api(self):
+        response = self.client.get(reverse('api:comment-list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_invalid(self):
+        response = self.client.get(reverse('api:comment-detail', args=(100,)))
+        self.assertEqual(response.status_code, 404)
